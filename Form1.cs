b9f89fe7acbbb80bddb5f63837c619e172397a9f
Nick Igelbrink <zi.aal2000@gmail.com>
2023-08-19 20:59:54 +0200

    minimal physics

--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,17 @@ namespace FangamePhysicsSimulator
 {
     public partial class Form1 : Form
     {
+        Player test = new Player(407, 0);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Y: {test.Y}, VSpeed: {test.VSpeed}");
+            test.Advance();
         }
     }
 }
---
+++ Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FangamePhysicsSimulator
+{
+    internal class Player
+    {
+        public const double GRAVITY = 0.4, RELEASE_MULTIPLIER = 0.45, SINGLEJUMP = 8.5, DOUBLEJUMP = 7, MAXVSPEED = 9;
+        public double Y = 407.4, VSpeed = 0;
+
+        public Player(double Y, double VSpeed)
+        {
+            this.Y = Y;
+            this.VSpeed = VSpeed;
+        }
+
+        // copy constructor
+        public Player(Player p)
+        {
+            Y = p.Y;
+            VSpeed = p.VSpeed;
+        }
+
+        public void Advance()
+        {
+            if (VSpeed > MAXVSPEED)
+                VSpeed = MAXVSPEED;
+
+            VSpeed += GRAVITY;
+            Y += VSpeed;
+
+            // ...
+        }
+    }
+}
