d5e16268c6cec81c661c1c50a4f8a00bfe229f8e
Nick Igelbrink <zi.aal2000@gmail.com>
2023-08-22 03:35:21 +0200

    more physics, strat to string (untested)

--- Player.cs
+++ Player.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace FangamePhysicsSimulator
 {
@@ -22,30 +20,37 @@ namespace FangamePhysicsSimulator
         static double _Floor;
         public static double Floor
         {
-            get { return _Floor; }
             set { _Floor = value - 8; }
         }
 
         static double _Ceiling;
         public static double Ceiling
         {
-            get { return _Ceiling; }
             set { _Ceiling = value - 12; }
         }
 
+        public bool Released;
         public double Y, VSpeed;
+        public int Frame;
+        List<Input> Inputs;
 
         public Player(double Y, double VSpeed)
         {
+            Released = false;
             this.Y = Y;
             this.VSpeed = VSpeed;
+            Frame = 0;
+            Inputs = new();
         }
 
         // copy constructor
         public Player(Player p)
         {
+            Released = p.Released;
             Y = p.Y;
             VSpeed = p.VSpeed;
+            Frame = p.Frame;
+            Inputs = new(p.Inputs);
         }
 
         public void Advance(bool Press, bool Release)
@@ -61,34 +66,38 @@ namespace FangamePhysicsSimulator
 
         void UpdateVSpeed(bool Press, bool Release)
         {
-            CheckPress(Press);
-
-            CheckRelease(Release);
+            CheckInputs(Press, Release);
 
             CapVSpeed();
 
             VSpeed += GRAVITY;
         }
 
-        void CheckPress(bool Press)
+        void CheckInputs(bool Press, bool Release)
         {
+            Input input = 0;
+
+            // force release if there was none when reaching positive vspeed
+            if (!Released && VSpeed > 0)
+            {
+                Release = true;
+                Released = true;
+            }
+
             if (Press)
             {
-                VSpeed = SINGLEJUMP;
+                VSpeed = -SINGLEJUMP;
+                input = Input.Press;
+                Released = false;
             }
-        }
-
-        void CheckRelease(bool Release)
-        {
             if (Release)
             {
-                // for the future
-                if (VSpeed < 0)
-                {
-                    throw new Exception("negative vspeed release");
-                }
                 VSpeed *= RELEASE_MULTIPLIER;
+                input |= Input.Release;
+                Released = true;
             }
+
+            Inputs.Add(input);
         }
 
         void CapVSpeed()
@@ -100,6 +109,68 @@ namespace FangamePhysicsSimulator
         void UpdatePosition()
         {
             Y += VSpeed;
+        }
+
+        [Pure]
+        string GetStrat()
+        {
+            StringBuilder sb = new();
+            int Frames = 0;
+            bool Held = false;
+
+            for (int i = 0; i < Inputs.Count; i++)
+            {
+                Input input = Inputs[i];
+                bool Press = (input & Input.Press) == Input.Press, 
+                    Release = (input & Input.Release) == Input.Release;
+
+                if (Press)
+                {
+                    if (Frames > 0)
+                    {
+                        if (Held)
+                        {
+                            sb.Append($"{Frames}f 0p ");
+                        }
+                        else
+                        {
+                            sb.Append($"{Frames}p ");
+                        }
+                    }
+
+                    Held = true;
+                    Frames = 0;
+                }
+                if (Release)
+                {
+                    if (Held)
+                    {
+                        sb.Append($"{Frames}f ");
+                    }
+                    else
+                    {
+                        sb.Append($"+{Frames} ");
+                    }
+
+                    Held = false;
+                    Frames = 0;
+                }
+
+                Frames++;
+            }
+
+            Frames--;
+
+            if (Held)
+            {
+                sb.Append($"{Frames}f ");
+            }
+            else if (Frames > 0)
+            {
+                sb.Append($" {Frames}p");
+            }
+
+            return sb.ToString().Trim();
         }
     }
 }
